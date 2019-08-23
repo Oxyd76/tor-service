174df2967cd7e62954557603dd7fd0aec6a9ff6d
Oxyd76 <oxyd76@gmail.com>
2019-08-23 04:04:18 +0300

    My fork of torrc and tor.service first version.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#	backup old torrc and tor.service
+sudo cp /usr/lib/systemd/system/tor.service ./`date +%d.%m.%y-%H_%M_%S`.tor.service
+sudo cp /ets/tor/torrc ./`date +%d.%m.%y-%H_%M_%S`.torrc
+
+sudo cp ./tor.service /usr/lib/systemd/system
+sudo chmod 644 /usr/lib/systemd/system/tor.service
+sudo cp ./torrc /etc/tor
+sudo chmod 644 /etc/tor/torrc
+sudo systemctl daemon-reload
